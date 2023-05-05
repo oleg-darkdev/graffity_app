8f314b45b628170b1debf77b5675f470bccaf141
DarkDev <darkdev_brand@riseup.net>
2023-05-05 14:11:56 +0300

    fix mks details

--- src/routes/app/persons/[slug]/+page.svelte
+++ src/routes/app/persons/[slug]/+page.svelte
@@ -1,27 +1,27 @@
 <script>
-	import { Article } from '../../../../lib/shared/';
-	import { RecommendationBlock } from '../../../../lib/entities';
+	// import { Article } from '../../../../lib/shared/';
+	// import { RecommendationBlock } from '../../../../lib/entities';
 
 	import { page } from '$app/stores';
-	import {
-		getCurrentUrl,
-		getLocationFromUrl,
-		identifySelectedData,
-		extensionsPersonsToc
-	} from '../../../../lib/shared/';
+	// import {
+	// 	getCurrentUrl,
+	// 	getLocationFromUrl,
+	// 	identifySelectedData,
+	// 	extensionsPersonsToc
+	// } from '../../../../lib/shared/';
 
-	const locationValue = getLocationFromUrl(getCurrentUrl(window), 2),
-		textArray = identifySelectedData(locationValue),
-		getThisArticleText = (textArray) => textArray.data[$page.params.slug - 1];
+	// const locationValue = getLocationFromUrl(getCurrentUrl(window), 2),
+	// 	textArray = identifySelectedData(locationValue),
+	// 	getThisArticleText = (textArray) => textArray.data[$page.params.slug - 1];
 </script>
 
-<Article manualArticle={getThisArticleText(textArray)} />
+<!-- <Article manualArticle={getThisArticleText(textArray)} /> -->
 
-<h2 class="mb-2 w-full text-center  text-6xl font-bold tracking-tighter  text-gray-100">
+<!-- <h2 class="mb-2 w-full text-center  text-6xl font-bold tracking-tighter  text-gray-100">
 	More persons in extensions
 </h2>
 <div class="mb-6 mb-4 grid w-full grid-cols-2 gap-2">
 	{#each extensionsPersonsToc.data as recommendation}
 		<RecommendationBlock link="/app/manual" {recommendation} />
 	{/each}
-</div>
+</div> -->
